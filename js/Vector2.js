9e107fdfea0e267ebde6ccc249161d306cd6ab5b
Manuel Rafael <109754744+manuelrafa@users.noreply.github.com>
2024-02-23 17:45:42 -0300

    refactor: métodos de Vector2
    - Añadidos los siguientes métodos de Vector2:
      > add()
      > sub()
      > mul()
      > div()
    
      > dot()
      > lenght()
      > distanceTo()
      > clone()
      > normalized()
    
      > static add()
      > static sub()
      > static mul()
      > static div()

--- js/Vector2.js
+++ js/Vector2.js
@@ -4,4 +4,96 @@ class Vector2 {
         this.x = x;
         this.y = y;
     }
+    /* Operaciones a sí mismo ======================================================================= */
+    /**
+     * Suma _v2_ a este vector.
+     */
+    add(v2) {
+        this.x += v2.x;
+        this.y += v2.y;
+    }
+    /**
+     * Resta _v2_ a este vector.
+     */
+    sub(v2) {
+        this.x -= v2.x;
+        this.y -= v2.y;
+    }
+    /**
+     * Multiplica este vector por _num_.
+     */
+    mul(num) {
+        this.x *= num;
+        this.y *= num;
+    }
+    /**
+     * Divide este vector por _num_.
+     */
+    div(num) {
+        this.x /= num;
+        this.y /= num;
+    }
+    /* Operaciones con return ======================================================================= */
+    /**
+     * Devuelve el producto escalar entre este vector y _v2_.
+     */
+    dot(v2) {
+        return this.x * v2.x + this.y * v2.y;
+    }
+    /**
+     * Devuelve la norma (magnitud) de este vector.
+     */
+    length() {
+        let len = Math.pow(this.x, 2) + Math.pow(this.y, 2);
+        return Math.sqrt(len);
+    }
+    /**
+     * Devuelve la distancia entre este vector y _v2_.
+     */
+    distanceTo(v2) {
+        v2.sub(this);
+        return Math.abs(v2.length());
+    }
+    /**
+     * Devuelve una copia de este vector.
+     */
+    clone() {
+        return new Vector2(this.x, this.y);
+    }
+    normalized() {
+        return Vector2.div(this, this.length());
+    }
+    /* Operaciones estáticas (no modifican al propio vector) ======================================== */
+    /**
+     * Devuelve la suma entre _v1_ y _v2_.
+     */
+    static add(v1, v2) {
+        const v3 = v1.clone();
+        v3.add(v2);
+        return v3;
+    }
+    /**
+     * Devuelve la resta entre _v1_ y _v2_.
+     */
+    static sub(v1, v2) {
+        const v3 = v1.clone();
+        v3.sub(v2);
+        return v3;
+    }
+    /**
+     * Devuelve el producto de _v_ por _num_.
+     */
+    static mul(v, num) {
+        const v2 = v.clone();
+        v2.mul(num);
+        return v2;
+    }
+    /**
+     * Devuelve la división de _v_ por _num_.
+     */
+    static div(v, num) {
+        const v2 = v.clone();
+        v2.div(num);
+        return v2;
+    }
 }
--- js/Vector2.ts
+++ js/Vector2.ts
@@ -1,8 +1,107 @@
 class Vector2 {
     x: number;
     y: number;
+    
     constructor(x: number = 0, y: number = 0) {
         this.x = x;
         this.y = y;
+    }
+
+    /* Operaciones a sí mismo ======================================================================= */
+
+    /**
+     * Suma _v2_ a este vector.
+     */
+    public add(v2: Vector2): void {
+        this.x += v2.x;
+        this.y += v2.y;
+    }
+    /**
+     * Resta _v2_ a este vector.
+     */
+    public sub(v2: Vector2): void {
+        this.x -= v2.x;
+        this.y -= v2.y;
+    }
+    /**
+     * Multiplica este vector por _num_.
+     */
+    public mul(num: number): void {
+        this.x *= num;
+        this.y *= num;
+    }
+    /**
+     * Divide este vector por _num_.
+     */
+    public div(num: number): void {
+        this.x /= num;
+        this.y /= num;
+    }
+
+    /* Operaciones con return ======================================================================= */
+
+    /**
+     * Devuelve el producto escalar entre este vector y _v2_.
+     */
+    public dot(v2: Vector2): number {
+        return this.x * v2.x + this.y * v2.y;
+    }
+    /**
+     * Devuelve la norma (magnitud) de este vector.
+     */
+    public length(): number {
+        let len = Math.pow(this.x, 2) + Math.pow(this.y, 2);
+        return Math.sqrt(len);
+    }
+    /**
+     * Devuelve la distancia entre este vector y _v2_.
+     */
+    public distanceTo(v2: Vector2): number {
+        v2.sub(this);
+        return Math.abs(v2.length());
+    }
+    /**
+     * Devuelve una copia de este vector.
+     */
+    public clone(): Vector2 {
+        return new Vector2(this.x, this.y);
+    }
+    public normalized(): Vector2 {
+        return Vector2.div(this, this.length());
+    }
+
+    /* Operaciones estáticas (no modifican al propio vector) ======================================== */
+
+    /**
+     * Devuelve la suma entre _v1_ y _v2_.
+     */
+    public static add(v1: Vector2, v2: Vector2): Vector2 {
+        const v3 = v1.clone();
+        v3.add(v2);
+        return v3;
+    }
+    /**
+     * Devuelve la resta entre _v1_ y _v2_.
+     */
+    public static sub(v1: Vector2, v2: Vector2): Vector2 {
+        const v3 = v1.clone();
+        v3.sub(v2);
+        return v3;
+    }
+    /**
+     * Devuelve el producto de _v_ por _num_.
+     */
+    public static mul(v: Vector2, num: number): Vector2 {
+        const v2 = v.clone();
+        v2.mul(num);
+        return v2;
+    }
+    /**
+     * Devuelve la división de _v_ por _num_.
+     */
+    public static div(v: Vector2, num: number): Vector2 {
+        const v2 = v.clone();
+        v2.div(num);
+        return v2;
     }
 }
